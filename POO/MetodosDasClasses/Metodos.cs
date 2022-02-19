790844d87259b6b6038a5b019f9b138ebdc22871
matosluciano <luciano7matos@gmail.com>
2022-02-19 16:03:32 -0300

    Passagem de parâmetros por valor e por referência

--- POO/MetodosDasClasses/Metodos.cs
+++ POO/MetodosDasClasses/Metodos.cs
@@ -8,5 +8,35 @@ namespace MetodosDasClasses
 {
     class Metodos
     {
+        // Métodos Simples
+        public void Cumprimentar()
+        {
+            Console.WriteLine("Olá, seja bem vindo !");
+        }
+
+        // Métodos com parâmetros
+        public void Somar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            Console.WriteLine("A soma é " + resultado);
+        }
+
+        public void Apresentar(string nome, int idade)
+        {
+            Console.WriteLine("Olá, meu nome é " + nome + " e tenho " + idade + " anos.");
+        }
+
+        // Passagem com parâmetros por valor
+        public void AumentarValor(int valor)
+        {
+            valor += 10;
+            Console.WriteLine("O valor final (por valor) é: " + valor);
+        }
+        // Passagem de parâmetros com referência
+        public void AumentarRef(ref int valor)
+        {
+            valor += 10;
+            Console.WriteLine("O valor final (por referência) é: " + valor);
+        }
     }
 }
--- POO/MetodosDasClasses/Program.cs
+++ POO/MetodosDasClasses/Program.cs
@@ -10,6 +10,22 @@ namespace MetodosDasClasses
     {
         static void Main(string[] args)
         {
+            Metodos m = new Metodos();
+
+            m.Cumprimentar();
+            m.Somar(50,60);
+            m.Apresentar("Luciano", 37);
+
+            int valor1 = 100;
+            int valor2 = 100;
+
+            m.AumentarValor(valor1);
+            m.AumentarRef(ref valor2);
+
+            Console.WriteLine("Valor 1: " + valor1);
+            Console.WriteLine("Valor 2: " + valor2);
+
+            Console.ReadKey();
         }
     }
 }
