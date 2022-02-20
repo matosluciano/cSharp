a868960699ec7a1e3adebedc147ab98b64365ffa
matosluciano <luciano7matos@gmail.com>
2022-02-20 20:19:12 -0300

    Membros estáticos

---
+++ POO/MetodoConstrutor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;       
+
+namespace MetodoConstrutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.nome = "Luciano";
+
+            Console.WriteLine(p1.nome);
+            Console.WriteLine(p1.sobrenome);
+            Console.WriteLine(p1.anoNascimento);
+            Console.WriteLine(p1.idade);
+
+            Console.WriteLine();
+
+            Pessoa p2 = new Pessoa("Luciano", "Matos", 1984);
+            Console.WriteLine(p2.nome);
+            Console.WriteLine(p2.sobrenome);
+            Console.WriteLine(p2.anoNascimento);
+            Console.WriteLine(p2.idade);
+
+            Console.WriteLine();
+
+            Pessoa p3 = new Pessoa("Gael", "Alana");
+            Console.WriteLine(p3.nome);
+            Console.WriteLine(p3.sobrenome);
+            Console.WriteLine(p3.anoNascimento);
+            Console.WriteLine(p3.idade);
+
+            Console.ReadKey();
+        }
+    }
+}
