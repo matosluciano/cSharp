e8fa00da7cbaff95501e2b20f19f00295eba275f
matosluciano <luciano7matos@gmail.com>
2022-02-20 18:19:27 -0300

    Classes estáticas

---
+++ POO/TipoDelagate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoDelagate
+{
+    class Program
+    {
+        delegate void Operacao(int num1, int num2);
+
+        static void Main(string[] args)
+        {
+            Matematica m = new Matematica();
+
+            Operacao conta = null;
+
+            conta += m.Somar;
+            conta += m.Subtrair;
+            conta += m.Multiplicar;
+            conta += m.Dividir;
+
+            conta(10,2);
+
+            conta -= m.Subtrair;
+            conta -= m.Dividir;
+
+            conta(10, 2);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
