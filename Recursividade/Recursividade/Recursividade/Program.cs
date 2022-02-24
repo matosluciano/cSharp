6b9cdfb24316519083f5d3261606f40128bc19e1
matosluciano <luciano7matos@gmail.com>
2022-02-24 13:19:34 -0300

    Mais de métodos

---
+++ Recursividade/Recursividade/Recursividade/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursiva r = new Recursiva();
+            
+            r.ExecutarRecursivo("Ola mundo!", 5);
+
+            Console.ReadKey();
+        }
+    }
+}
