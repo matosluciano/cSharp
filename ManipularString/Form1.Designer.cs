3f59822b9b80b0b28553db0e71a8b234b1fbda0d
matosluciano <luciano7matos@gmail.com>
2022-03-27 01:02:53 -0300

    Update Strings

---
+++ ManipularString/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace ManipularString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnexecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(560, 209);
+            this.label1.TabIndex = 0;
+            // 
+            // btnexecutar
+            // 
+            this.btnexecutar.Location = new System.Drawing.Point(451, 291);
+            this.btnexecutar.Name = "btnexecutar";
+            this.btnexecutar.Size = new System.Drawing.Size(120, 39);
+            this.btnexecutar.TabIndex = 1;
+            this.btnexecutar.Text = "Executar";
+            this.btnexecutar.UseVisualStyleBackColor = true;
+            this.btnexecutar.Click += new System.EventHandler(this.btnexecutar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 342);
+            this.Controls.Add(this.btnexecutar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnexecutar;
+    }
+}
+
---
+++ ManipularString/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipularString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnexecutar_Click(object sender, EventArgs e)
+        {
+            #region Contains / ToString
+            /*string texto = "Este tipo de variável é alfanumérica.";
+
+            if (texto.Contains("Tipo"))
+            {
+                label1.Text = "Contém.";
+            }
+            else
+            {
+                label1.Text = "Não Contém."; ;
+            }*/
+            int num = 5400;
+            int val = 2000;
+            label1.Text = num.ToString();
+            label1.Text = val.ToString();
+            int res = num + val;
+
+            label1.Text = res.ToString();
+
+            Console.WriteLine(res);
+
+            #endregion
+
+            #region ToUpper / ToLower
+            string nome = "Luciano";
+
+            //nome = nome.ToUpper(); mesmo efeito do código abaixo:
+            // string nomeMaiusculo = nome.ToUpper();
+
+
+
+            label1.Text = nome.ToLower();
+            #endregion
+
+
+
+        }
+    }
+}
